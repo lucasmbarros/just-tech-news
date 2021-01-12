94ebd18b17958f111585977a83a8ae3bce23f331
Lucas Barros <lucasmello.barros@gmail.com>
2021-01-12 00:08:34 -0500

    create the post model

---
+++ models/Post.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+// create our post Model
+class Post extends Model {}
+
+// creates fields/columns for Post model
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isUrl: true,
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTablename: true,
+    underscored: true,
+    modelName: "post",
+  }
+);
+
+module.exports = Post;
