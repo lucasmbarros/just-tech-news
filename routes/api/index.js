5024284dec23bf15ec703728295988cb091c3c4e
Lucas Barros <lucasmello.barros@gmail.com>
2021-01-14 00:49:12 -0500

    add post routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
-const router = require('express').Router();
-const UserRoutes = require('./user-routes');
+const router = require("express").Router();
+const userRoutes = require("./user-routes");
+const postRoutes = require("./post-routes");
 
-router.use('/users', UserRoutes);
+router.use("/users", userRoutes);
+router.use("/posts", postRoutes);
 
 module.exports = router;
