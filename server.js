f6bb7826a01624c4620c76153b8cf4a83872de32
Lucas Barros <lucasmello.barros@gmail.com>
2021-01-30 13:47:28 -0500

    format dates,url,points,comments and refactor

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const path = require("path");
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create({});
 const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const helpers = require("./utils/helpers");
 
 const sess = {
   secret: "Birds work for the rich",
@@ -16,6 +16,8 @@ const sess = {
     db: sequelize,
   }),
 };
+
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
